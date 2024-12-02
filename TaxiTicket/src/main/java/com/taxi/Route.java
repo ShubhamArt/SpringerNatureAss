1eb678866da62ed6024270c719bd7d7927063e23
Shubham-26Rp <shubh.shete77@gmail.com>
2024-12-02 22:41:22 +0530

    second commit : added implementation to make test pass and run the test

---
+++ TaxiTicket/src/main/java/com/taxi/Route.java
@@ -0,0 +1,25 @@
+package com.taxi;
+
+public class Route {
+    private String from;
+    private String to;
+    private int distance;
+
+    public Route(String from, String to, int distance) {
+        this.from = from;
+        this.to = to;
+        this.distance = distance;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
--- TaxiTicket/src/main/java/com/taxi/TaxiTicket.java
+++ TaxiTicket/src/main/java/com/taxi/TaxiTicket.java
@@ -1,10 +1,21 @@
 package com.taxi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaxiTicket {
     private String source;
     private String destination;
     private int travellers;
 
+    private static List<Route> routes = new ArrayList<>();
+
+    static {
+        routes.add(new Route("PUNE", "MUMBAI", 120));
+        routes.add(new Route("PUNE", "NASIK", 200));
+        routes.add(new Route("MUMBAI", "GOA", 350));
+        routes.add(new Route("MUMBAI", "NASIK", 180));
+    }
     public TaxiTicket(String source, String destination, int travellers) {
         this.source = source;
         this.destination = destination;
@@ -19,11 +30,21 @@ public class TaxiTicket {
                 source, destination, distance, travellers, totalFare);
     }
     private int getDistance() {
+        for (Route route : routes) {
+            if ((route.getFrom().equals(source) && route.getTo().equals(destination)) ||
+                    (route.getFrom().equals(destination) && route.getTo().equals(source))) {
+                return route.getDistance();
+            }
+        }
         return 0;
     }
 
     private int calculateFare(int distance) {
-        return 0;
+        if (distance <= 100) {
+            return 750;
+        } else {
+            return 750 + (distance - 100) * 5;
+        }
     }
 
 }
